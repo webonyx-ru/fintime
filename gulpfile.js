d280edb7d5d0e62a53875b0370d43eb1711e05e2
Yuriy Bogatyrenko <yurabogatyrenko@gmail.com>
2016-10-03 19:56:40 +0600

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -93,7 +93,7 @@ gulp.task('bower', function () {
 gulp.task('connect', function () {
     connect.server({
         root: 'app',
-        port: 3000,
+        port: 30010,
         livereload: true
     });
 });
